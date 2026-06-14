d391273ffb4f92f6fcbb83fd8631cf02eb23c164
Mohammad Nourian <mhn1999@gmail.com>
2026-06-14 23:40:01 +0100

    sync local and remote repo

--- src/protocols/http/HttpSerializer.cpp
+++ src/protocols/http/HttpSerializer.cpp
@@ -6,7 +6,17 @@ namespace ProtocolDispatcher::protocols::http {
 
 std::string HttpSerializer::serialize(const HttpRequest& request) const {
     std::ostringstream stream;
-    stream << to_string(request.method) << ' ' << request.path << " HTTP/1.1\r\n";
+    if (request.query_params.empty()) {
+        stream << to_string(request.method) << ' ' << request.path << " HTTP/1.1\r\n";
+    }
+    else {
+        std::ostringstream params;
+        for (const HttpQueryParam& param : request.query_params) {
+            params << param.name << "=" << param.value << "\r\n";
+        }
+        stream << to_string(request.method) << ' ' << request.path << "?" << params.str() <<" HTTP/1.1\r\n";
+    }
+
     stream << "Host: " << request.host << "\r\n";
 
     for (const auto& header : request.headers) {
